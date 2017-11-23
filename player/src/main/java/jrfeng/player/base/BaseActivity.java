af206e7f6d9862bd761c002de5f343750b977ea0
jrfeng <949305241@qq.com>
2017-11-23 19:30:50 +0800

    修复可能导致会内存泄露的代码

--- player/src/main/java/jrfeng/player/base/BaseActivity.java
+++ player/src/main/java/jrfeng/player/base/BaseActivity.java
@@ -18,7 +18,7 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityStack.push(this);
+        ActivityStack.add(this);
         if (savedInstanceState != null && !MusicPlayerClient.getInstance().isConnect()) {
             resetApp();
         }
@@ -27,7 +27,7 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        ActivityStack.pop(this);
+        ActivityStack.remove(this);
     }
 
     //****************private**************
--- player/src/main/java/jrfeng/player/utils/activity/ActivityStack.java
+++ player/src/main/java/jrfeng/player/utils/activity/ActivityStack.java
@@ -6,16 +6,19 @@ import java.util.List;
 import jrfeng.player.base.BaseActivity;
 
 public class ActivityStack {
-    private static List<BaseActivity> mStack = new LinkedList<>();
+    private static List<BaseActivity> mStack;
 
     private ActivityStack() {
     }
 
-    public static void push(BaseActivity activity) {
+    public synchronized static void add(BaseActivity activity) {
+        if (mStack == null) {
+            mStack = new LinkedList<>();
+        }
         mStack.add(activity);
     }
 
-    public static void pop(BaseActivity activity) {
+    public static void remove(BaseActivity activity) {
         mStack.remove(activity);
     }
 
@@ -26,5 +29,6 @@ public class ActivityStack {
             }
         }
         mStack.clear();
+        mStack = null;
     }
 }
